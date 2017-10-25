392b66905ddd0cdfd77b81bdd9975b6f5c5fa751
richardmckenzie <richard.mckenzie@gmail.com>
2017-10-25 21:20:00 +1300

    test

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ var express = require('express')
 
         //CORS middleware
       var allowCrossDomain = function(req, res, next) {
-          res.header('Access-Control-Allow-Origin', 'pacific-river-35283.herokuapp.com');
-          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-          res.header('Access-Control-Allow-Headers', 'Content-Type');
-
+        if ('OPTIONS' == req.method) {
+          res.header('Access-Control-Allow-Origin', '*');
+          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+          res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+          res.send(200);
+        }
+        else {
           next();
+        }
       }
 
       //...

941ba213f34797762d8b299bd52763a79d974bd2
richardmckenzie <richard.mckenzie@gmail.com>
2017-10-25 20:23:39 +1300

    test

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const url = process.env.MONGOLAB_URI;
 var express = require('express')
         , app = express();
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://warm-mesa-85711.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE"); // If needed
+        //CORS middleware
+      var allowCrossDomain = function(req, res, next) {
+          res.header('Access-Control-Allow-Origin', 'example.com');
+          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+          res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
-  });
+          next();
+      }
+
+      //...
+      app.configure(function() {
+          app.use(express.bodyParser());
+          app.use(allowCrossDomain);
+          app.use(app.router);
+      });
 
   app.use(require("body-parser").json());
 

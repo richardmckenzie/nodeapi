4390b679f42495a171fb8e698ababa0f98f4f3d1
richardmckenzie <richard.mckenzie@gmail.com>
2017-10-25 20:17:24 +1300

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var express = require('express')
         , app = express();
 
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://pacific-river-35283.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE"); // If needed
 

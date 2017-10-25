8d5c2649412d83757c11647808bcf556f4cb8254
richardmckenzie <richard.mckenzie@gmail.com>
2017-10-25 21:36:29 +1300

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var express = require('express')
       var allowCrossDomain = function(req, res, next) {
           res.header('Access-Control-Allow-Origin', '*');
           res.header('Access-Control-Allow-Methods', '*');
-          res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+          res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');;
 
           next();
 

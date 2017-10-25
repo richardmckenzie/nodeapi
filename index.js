107528d8510ad648649526766e364a623f819dc2
Richard McKenzie <richard_mckenzie@bnz.co.nz>
2017-10-25 14:42:20 +1300

    overcome cors issue

--- index.js
+++ index.js
@@ -26,15 +26,21 @@ app.get('/api/signatures', function(req, res) {
   })
 //==========================//
 //====POST NEW SIGNATURE===//
-app.post('/api/signatures', bodyParser, function(req, res) {
-  Signature.create({
-    guestSignature: req.body.SignatureOfGuest,
-    message: req.body.MessageOfGuest,
-  }).then(signature => {
-res.header('Access-Control-Allow-Origin', '*');
-res.header('Access-Control-Allow-Methods', 'GET,PUT, POST,DELETE');    
-res.json(signature)
-  });
+app.post('/api/signatures', function(req, res) {
+//  Signature.create({
+//    guestSignature: req.body.SignatureOfGuest,
+//    /message: req.body.MessageOfGuest,
+//  }).then(signature => {
+//res.header('Access-Control-Allow-Origin', '*');
+//res.header('Access-Control-Allow-Methods', 'GET,PUT, POST,DELETE');
+//res.json(signature)
+//  });
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+res.send('cors problem fixed:)');
 });
 //==========================//
 

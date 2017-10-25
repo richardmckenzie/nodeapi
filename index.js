8c8d850f5076990d6d33219a7cd2bef5adb60ce4
richardmckenzie <richard.mckenzie@gmail.com>
2017-10-25 21:30:40 +1300

    test

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ var express = require('express')
 
         //CORS middleware
       var allowCrossDomain = function(req, res, next) {
-        if ('OPTIONS' == req.method) {
           res.header('Access-Control-Allow-Origin', '*');
-          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-          res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-          res.send(200);
-        }
-        else {
+          res.header('Access-Control-Allow-Methods', '*');
+          res.header('Access-Control-Allow-Headers', '*');
+
           next();
-        }
+
       }
 
       //...

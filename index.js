9cc7085802051a2bd4aa3f7e7ed2db50c4c2e398
Richard McKenzie <richard_mckenzie@bnz.co.nz>
2017-11-07 11:45:48 +1300

    adding url

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api/signatures', function(req, res) {
 //==========================//
 //====POST NEW SIGNATURE===//
 app.post('/api/signatures', function(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.flosflowers.co.nz');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed

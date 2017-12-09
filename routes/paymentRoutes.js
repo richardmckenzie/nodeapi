116b33eaeac8601b53d0463242ceb74cbadda4e3
richardmckenzie <richard.mckenzie@gmail.com>
2017-12-10 07:40:48 +1300

    debug

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,6 +5,7 @@ const requireLogin = require("../middlewares/requireLogin");
 module.exports = app => {
   app.post("/api/stripe", requireLogin, async (req, res) => {
     console.log(req.body);
+    console.log(req.headers);
 
     const charge = await stripe.charges.create({
       amount: 500,

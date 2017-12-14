e0baf477dd307720883836ab07f58a17ba2a34d1
richardmckenzie <richard.mckenzie@gmail.com>
2017-12-15 06:43:05 +1300

    log headers

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,20 +4,18 @@ const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
   app.post("/api/stripe", requireLogin, async (req, res) => {
-    req.headers = {};
-
-    console.log(req.headers);
+    console.log(JSON.stringify(req.headers));
 
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
-      description: "blah",
+      description: "$5 for 5 credits",
       source: req.body.id
     });
 
     req.user.credits += 5;
-
     const user = await req.user.save();
+
     res.send(user);
   });
 };
